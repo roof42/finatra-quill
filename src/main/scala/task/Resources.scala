3456a8eb6e2014fd633dd54ae0afd6261c3ba8d9
roof <roof@odds.team>
2022-03-31 23:49:58 +0700

    Provide proper return message

--- src/main/scala/task/Resources.scala
+++ src/main/scala/task/Resources.scala
@@ -3,16 +3,26 @@ import com.twitter.finatra.http.Controller
 import com.twitter.finagle.http.Request
 import com.google.inject.Inject
 import task._
-class Resources @Inject()(repo: ArrayBufferRepo) extends Controller {
-  get("/ping")    { _: Request => response.accepted("pong") }
+class Resources @Inject() (repo: ArrayBufferRepo) extends Controller {
+  get("/ping") { _: Request => response.accepted("pong") }
 
-  get("/todos")   { _: Request => repo.getAllTodos() }
-  post("/todos")  { plan: Plan =>
+  get("/todos") { _: Request => repo.getAllTodos() }
+  post("/todos") { plan: Plan =>
     val todo = repo.createTodo(plan)
     response.created(s"Todo was created with id = ${todo.id}")
   }
-  post("/todos/next") { todo: Todo => repo.next(todo) }
+  post("/todos/next") { todo: Todo =>
+    println(s"About to move ${todo.id} - ${todo.detail}")
+    repo.next(todo) match {
+      case Some(doing) =>
+        response.created(
+          s"Task number ${doing.asInstanceOf[Doing].id} was moved to doing list"
+        )
+      case None => response.badRequest("Illegal id")
+    }
 
-  get("/doings")      { _: Request => repo.getAllDoings() }
+  }
+
+  get("/doings") { _: Request => repo.getAllDoings() }
   post("/doing/next") { doing: Doing => repo.next(doing) }
 }
