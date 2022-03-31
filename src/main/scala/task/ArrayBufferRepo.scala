b0598762e9e7f51ca26f202ca56bcc8a2fc6f807
roof <roof@odds.team>
2022-03-31 20:23:45 +0700

    change return type

--- src/main/scala/task/ArrayBufferRepo.scala
+++ src/main/scala/task/ArrayBufferRepo.scala
@@ -1,32 +1,40 @@
 package task
+
 import scala.collection.mutable.ArrayBuffer
 
-class ArrayTaskRepo {
+class ArrayBufferRepo {
   val todoList: ArrayBuffer[Todo] = ArrayBuffer.empty[Todo]
   val doingList: ArrayBuffer[Doing] = ArrayBuffer.empty[Doing]
   val doneList: ArrayBuffer[Done] = ArrayBuffer.empty[Done]
 
-  def createTodo(plan: Plan): Int = {
-    val id = todoList.last.id + 1
-    todoList += Todo(id, plan.detail)
-    doingList.sortWith(_.id < _.id)
-    id
-  }
-
   def getAllTodos():ArrayBuffer[Todo] = todoList
   def getAllDoings() :ArrayBuffer[Doing]= doingList
+
+  def createTodo(plan: Plan): Todo = {
+    val newTodo = todoList.lastOption match {
+      case Some(todo) => Todo(todo.id+1, plan.detail)
+      case None => Todo(1, plan.detail)
+    }
+    todoList +=newTodo
+    newTodo
+  }
+
 
   def next(task: Task):Task = {
     task match {
       case Todo(id, detail, _) =>
         val todo = todoList.filter(_.id == id).apply(0)
-        doingList += Doing(id, detail)
+        val doing = Doing(id, detail)
+        doingList += doing
         todoList.remove(todoList.indexOf(todo))
+        doing
       case Doing(id, detail, _) =>
         val doing = doingList.filter(_.id == id).apply(0)
-        doneList += Done(id, detail)
+        val done = Done(id, detail)
+        doneList += done
         doingList.remove(doingList.indexOf(doing))
-      case Done(_, _, _) => task
+        done
+      case Done(_, _, _) => task.asInstanceOf[Done]
     }
   }
 }
