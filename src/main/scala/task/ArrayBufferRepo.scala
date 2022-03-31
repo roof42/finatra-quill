3b718c94179bc70368fa4cdaa72b5c7ef73d9158
roof <roof@odds.team>
2022-03-31 23:51:05 +0700

    Change storage type to Map

--- src/main/scala/task/ArrayBufferRepo.scala
+++ src/main/scala/task/ArrayBufferRepo.scala
@@ -1,40 +1,53 @@
 package task
 
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.Map
 
 class ArrayBufferRepo {
-  val todoList: ArrayBuffer[Todo] = ArrayBuffer.empty[Todo]
-  val doingList: ArrayBuffer[Doing] = ArrayBuffer.empty[Doing]
-  val doneList: ArrayBuffer[Done] = ArrayBuffer.empty[Done]
+  val todoList: Map[Int, Todo] = Map.empty[Int, Todo]
+  val doingList: Map[Int, Doing] = Map.empty[Int, Doing]
+  val doneList: Map[Int, Done] = Map.empty[Int, Done]
 
-  def getAllTodos():ArrayBuffer[Todo] = todoList
-  def getAllDoings() :ArrayBuffer[Doing]= doingList
+  def getAllTodos(): Map[Int, Todo] = todoList
+  def getAllDoings(): Map[Int, Doing] = doingList
 
   def createTodo(plan: Plan): Todo = {
     val newTodo = todoList.lastOption match {
-      case Some(todo) => Todo(todo.id+1, plan.detail)
-      case None => Todo(1, plan.detail)
+      case Some(value) => Todo(value._1.intValue() + 1, plan.detail)
+      case None        => Todo(0, plan.detail)
     }
-    todoList +=newTodo
+    todoList += (newTodo.id -> newTodo)
     newTodo
   }
 
-
-  def next(task: Task):Task = {
+  def next(task: Task): Option[Task] = {
     task match {
-      case Todo(id, detail, _) =>
-        val todo = todoList.filter(_.id == id).apply(0)
-        val doing = Doing(id, detail)
-        doingList += doing
-        todoList.remove(todoList.indexOf(todo))
-        doing
-      case Doing(id, detail, _) =>
-        val doing = doingList.filter(_.id == id).apply(0)
-        val done = Done(id, detail)
-        doneList += done
-        doingList.remove(doingList.indexOf(doing))
-        done
-      case Done(_, _, _) => task.asInstanceOf[Done]
+      case Todo(id, _, _) =>
+        moveTaskToDoing(id)
+      case Doing(id, _, _) =>
+        moveTaskToDone(id)
+      case _ => Some(task.asInstanceOf[Done])
+    }
+  }
+
+  private def moveTaskToDone(id: Int): Option[Done] = {
+    doingList.get(id) match {
+      case Some(value) =>
+        val done = Done(value.id, value.detail)
+        doneList += (done.id -> done)
+        doingList.remove(value.id)
+        Some(done)
+      case None => None
+    }
+  }
+
+  private def moveTaskToDoing(id: Int): Option[Doing] = {
+    todoList.get(id) match {
+      case Some(value) =>
+        val doing = Doing(value.id, value.detail)
+        doingList += (doing.id -> doing)
+        todoList.remove(value.id)
+        Some(doing)
+      case None => None
     }
   }
 }
