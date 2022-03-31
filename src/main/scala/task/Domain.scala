3f81d5b39bf023f3fb082612af9772c152e7898a
roof <roof@odds.team>
2022-03-31 23:50:25 +0700

    Move id back to Task

--- src/main/scala/task/Domain.scala
+++ src/main/scala/task/Domain.scala
@@ -2,24 +2,22 @@ package task
 
 import java.time.Instant
 
-trait Id {
-  val id: Int
-}
 
 trait Task {
+  val id: Int
   val detail: String
   val postedAt: Instant
 }
 
 case class Todo(id: Int, detail: String, postedAt: Instant = Instant.now())
     extends Task
-    with Id
+
 case class Doing(id: Int, detail: String, postedAt: Instant = Instant.now())
     extends Task
-    with Id
+
 case class Done(id: Int, detail: String, postedAt: Instant = Instant.now())
     extends Task
-    with Id
 
-case class Plan(detail: String, postedAt: Instant = Instant.now()) extends Task
+
+case class Plan(detail: String, postedAt: Instant = Instant.now())
 
