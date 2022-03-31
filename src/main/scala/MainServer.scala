859c0231156fcbdc3bf1e708f7a95f211b463ee7
roof <roof@odds.team>
2022-03-31 20:24:29 +0700

    Change from controller to resource

--- src/main/scala/MainServer.scala
+++ src/main/scala/MainServer.scala
@@ -1,11 +1,11 @@
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.routing.HttpRouter
-import task.TaskController
+import task.Resources
 
 object TaskMainServer extends MainServer
 
 class MainServer extends HttpServer {
   protected def configureHttp(router: HttpRouter): Unit = {
-    router.add[TaskController]
+    router.add[Resources]
   }
 }
