19e854978acbb82e59d00110f65f2d07641977e3
roof <roof@odds.team>
2022-04-04 07:52:32 +0700

    Basic test

--- src/test/scala/task/ArrayBufferRepoSpec.scala
+++ src/test/scala/task/ArrayBufferRepoSpec.scala
@@ -1,27 +1,22 @@
 package task
 
-package task
 import org.scalatest.flatspec.AnyFlatSpec
 import scala.collection.mutable.Map
 
 class ArrayBufferRepoSpec extends AnyFlatSpec {
   it should "create new todo with id = 0" in {
     val plan = Plan("Buy banana")
-    val repo = new ArrayBufferRepo()
+    val repo = ArrayBufferRepo()
     val todo = repo.createTodo(plan)
     assert(todo.isInstanceOf[Todo])
     assert(todo.id == 0)
   }
 
   it should "move todo to doing with same id" in {
-    val plan = Plan("Buy banana")
-    val repo = new ArrayBufferRepo(
+    val repo = ArrayBufferRepo(
       todoList = Map(0 -> Todo(id = 0, detail = "buy banana"))
     )
-    val todo = repo.getAllTodos().get(0) match {
-      case Some(value) => value
-      case None        => ???
-    }
+    val todo = repo.getAllTodos().head
 
     val doing = repo.next(todo)
 
@@ -29,16 +24,12 @@ class ArrayBufferRepoSpec extends AnyFlatSpec {
   }
 
   it should "has empty todoList after move 1 item out" in {
-    val plan = Plan("Buy banana")
-    val repo = new ArrayBufferRepo(
+    val repo =  ArrayBufferRepo(
       todoList = Map(0 -> Todo(id = 0, detail = "buy banana"))
     )
-    val todo = repo.getAllTodos().get(0) match {
-      case Some(value) => value
-      case None        => ???
-    }
+    val todo = repo.getAllTodos().head
 
-    val doing = repo.next(todo)
+    repo.next(todo)
 
     assert(repo.getAllTodos().size == 0)
     assert(repo.getAllDoings().size == 1)
