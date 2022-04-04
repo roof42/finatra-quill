046ae35a058e41408d46d619a0c4055cc7cd5aff
roof <roof@odds.team>
2022-04-04 21:03:07 +0700

    Test ping api

--- src/test/scala/HelloControllerSpec.scala
+++ src/test/scala/HelloControllerSpec.scala
@@ -1,18 +1,12 @@
-package task
-
-import com.twitter.finatra.http.EmbeddedHttpServer
 import com.twitter.finagle.http.Status
+import com.twitter.finatra.http.EmbeddedHttpServer
 import com.twitter.inject.server.FeatureTest
 import scala.collection.immutable.ListMap
 
 class HelloControllerSpec extends FeatureTest {
-  override val server = new EmbeddedHttpServer(
-    twitterServer = new MainServer
-    // globalFlags = ListMap(com.foo.bar.someGlobalFlag -> "a value")
-  )
+  override val server = new EmbeddedHttpServer(twitterServer = new MainServer)
 
   test("ExampleServer#perform feature") {
-    // any read of the `com.foo.bar.someGlobalFlag` value within the server will be "a value"
-    server.httpGet(path = "/", andExpect = Status.Ok)
+    server.httpGet(path = "/ping", andExpect = Status.Accepted)
   }
 }
