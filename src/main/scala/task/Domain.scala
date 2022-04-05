a531b28280db2dea6636a456eb86f565b7e2fe9f
roof <roof@odds.team>
2022-04-05 22:54:48 +0700

    Remove unused method

--- src/main/scala/task/Domain.scala
+++ src/main/scala/task/Domain.scala
@@ -18,11 +18,3 @@ case class Done(id: Int, detail: String, postedAt: Instant = Instant.now())
     extends Task
 
 case class Plan(detail: String, postedAt: Instant = Instant.now())
-
-
-object Doing {
-  def ->:(td: Option[Todo]): Option[Doing]  = td match {
-    case Some(todo) => Some(Doing(todo.id, todo.detail, todo.postedAt))
-    case None => None
-  }
-}
